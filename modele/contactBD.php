4eb6d6bda8871a058675172a08d17cbd952626f8
madskullkoa <32908247+madskullkoa@users.noreply.github.com>
2021-10-04 17:30:00 +0200

    First Commit

---
+++ modele/contactBD.php
@@ -0,0 +1,28 @@
+﻿<?php 
+	/*Fonctions-modèle réalisant les requètes de gestion des contacts en base de données*/
+	
+	// liste_contact_bd : retourne la liste des informations pour chaque contact de l'utilisateur $id
+	
+	function contacts($idn) {
+		require ("Modele/connectBD.php") ; 
+		$sql="SELECT nom, prenom, email FROM contact c, utilisateur u 
+		WHERE c.id_nom=:id_nom AND c.id_contact = u.id_nom
+		LIMIT 0,30"; // LIMIT ne marche pas en MS SQL SERVER
+		try {
+			$commande = $pdo->prepare($sql);
+			$commande->bindParam(':id_nom', $idn);
+			$bool = $commande->execute();
+			$C= array();
+			if ($bool) {
+				while ($c = $commande->fetch()) {
+					$C[] = $c; //stockage dans $C des enregistrements sélectionnés
+				}	
+			}
+		}
+		catch (PDOException $e) {
+			echo utf8_encode("Echec de select : " . $e->getMessage() . "\n");
+			die(); // On arrête tout.
+		}
+		return $C;
+	}
+?>
---
+++ vue/utilisateur/ident.tpl
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>TP1 contacts - mvc - formulaire d'identification</title>
+		<link rel="stylesheet" href="vue/styleCSS/utilisateur.css"/>
+		<!-- <script src="script.js"></script> -->
+	</head>
+	<body>
+		<form action="index.php?controle=utilisateur&action=ident" method="post">
+			<label>nom :</label>
+			<input name="nom" class="nom" value="<?php echo $nom ?>" /> 
+			<label>identifiant :</label>
+			<input name="num" class="num" value="<?php echo $num ?>" />
+			<input type= "submit" value= "ok" /> 	 
+		</form>
+		<div id ="m"> <?php echo $msg; ?> </div>
+	</body>
+</html>
